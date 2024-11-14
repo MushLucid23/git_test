78d3ecdbf998a1eaebe4596be9930f1ced8ae61b
152053575+MushLucid23@users.noreply.github.com <152053575+MushLucid23@users.noreply.github.com>
2024-11-13 19:02:24 -0500

    Created new files to practice js
    Created new index, script and style files to practice LOOPs

---
+++ script.js
@@ -0,0 +1,51 @@
+// let i = 0;
+
+// while(i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+// let i = 1;
+
+// do{
+//     console.log(i);
+//     i++;
+// } while(i <= 5);
+
+for (let i = 1; i <= 5; i++) {
+
+        if(i == 3) {
+            continue; /*break*/
+        }
+         console.log(i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//FOR LOOPS! for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+    
+//     for(let j = 1; j <= 3; j++) {
+//         console.log("Inner Loop " + j);
+//     }
+// }
+
+// let coding = ["JavaScript", "Python", "CPP"]
+
+// for(let i = 0; i < coding.length; i++) {
+//     console.log(coding[i]);
+//}
